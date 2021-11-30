a5e6801e781af1d06a10685ccc21d92c5fd9d220
Siddhanta <siddhanta.c@gmail.com>
2021-11-30 22:58:52 +0800

    Start api work

--- cmd/main.go
+++ cmd/main.go
@@ -1,39 +1,54 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"time"
+	// "log"
+	"net/http"
+	// "time"
 
-	"github.com/siddhantac/fintra/domain"
+	"github.com/siddhantac/fintra/api"
+	// "github.com/siddhantac/fintra/domain"
 	"github.com/siddhantac/fintra/infra/store"
+	"github.com/siddhantac/fintra/repository"
 )
 
+// func main() {
+// 	ms := store.NewMemStore()
+
+// 	tx, err := domain.NewTransaction(23, time.Now(), true, string(domain.TrCategoryEntertainment), string(domain.TrTypeExpense), "movies", "Citibank")
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	ms.Insert(tx.ID, tx)
+
+// 	tx2, err := domain.NewTransaction(11, time.Now(), true, string(domain.TrCategoryMeals), string(domain.TrTypeExpense), "foodpanda", "Citibank")
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	ms.Insert(tx2.ID, tx2)
+
+// 	tx3, err := domain.NewTransaction(12, time.Now(), true, string(domain.TrCategoryMeals), string(domain.TrTypeExpense), "deliveroo", "Citibank")
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	ms.Insert(tx3.ID, tx3)
+
+// 	alltx := ms.GetAll()
+
+// 	fmt.Println("no. of txn: ", len(alltx))
+// 	for _, tx := range alltx {
+// 		fmt.Printf("%v\n", tx)
+// 	}
+// }
+
 func main() {
-	ms := store.NewMemStore()
+	// tx, err := domain.NewTransaction(23, time.Now(), true, string(domain.TrCategoryEntertainment), string(domain.TrTypeExpense), "movies", "Citibank")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
-	tx, err := domain.NewTransaction(23, time.Now(), true, string(domain.TrCategoryEntertainment), string(domain.TrTypeExpense), "movies", "Citibank")
-	if err != nil {
-		log.Fatal(err)
-	}
-	ms.Insert(tx.ID, tx)
+	txnRepo := repository.NewTransactionRepository(store.NewMemStore())
+	h := api.CreateTransaction(txnRepo)
 
-	tx2, err := domain.NewTransaction(11, time.Now(), true, string(domain.TrCategoryMeals), string(domain.TrTypeExpense), "foodpanda", "Citibank")
-	if err != nil {
-		log.Fatal(err)
-	}
-	ms.Insert(tx2.ID, tx2)
-
-	tx3, err := domain.NewTransaction(12, time.Now(), true, string(domain.TrCategoryMeals), string(domain.TrTypeExpense), "deliveroo", "Citibank")
-	if err != nil {
-		log.Fatal(err)
-	}
-	ms.Insert(tx3.ID, tx3)
-
-	alltx := ms.GetAll()
-
-	fmt.Println("no. of txn: ", len(alltx))
-	for _, tx := range alltx {
-		fmt.Printf("%v\n", tx)
-	}
+	http.HandleFunc("/transaction", h)
+	http.ListenAndServe("", nil)
 }
